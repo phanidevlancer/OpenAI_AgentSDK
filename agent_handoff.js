d92954f79a0c4ff095cb5e25cba51a687ef2f942
Phanindra <phanindra@Phanindras-MacBook-Pro.local>
2026-04-08 11:32:09 +0530

    handoff pattern with RECOMMENDED_PROMPT_PREFIX

--- agent_handoff.js
+++ agent_handoff.js
@@ -1,8 +1,8 @@
 import 'dotenv/config'
-import { Agent, run, tool } from '@openai/agents'
+import { Agent, tool, run } from '@openai/agents'
+import { RECOMMENDED_PROMPT_PREFIX } from '@openai/agents-core/extensions';
 import { z } from 'zod'
 import fs from 'fs/promises'
-import { tr } from 'zod/locales'
 
 
 const plans = [
@@ -42,7 +42,7 @@ const refundTool = tool({
 const refundAgent = new Agent({
     name: 'Refund Agent',
     instructions: 'You are an expert refund agent, who understands user query and process the user refund gracefully',
-    tools : [refundTool]
+    tools: [refundTool]
 
 })
 
@@ -52,15 +52,29 @@ const salesAgent = new Agent({
     instructions: `You are an expret sales agent for a internet broadband company named Beyond Internet.
     Talk to the user and help them with what they need`,
     tools: [fetchAvailablePlans, refundAgent.asTool({
-        toolName : 'refund expert',
-        toolDescription : 'handles user refunds gracefully'
+        toolName: 'refund expert',
+        toolDescription: 'handles user refunds gracefully'
     })]
 })
 
 
+const customerFacingAgent = new Agent({
+    name: "Customer facing agent",
+    instructions: `${RECOMMENDED_PROMPT_PREFIX}
+            You are an expert customer-facing agent who understands user queries and gracefully handoff their request to the respective agents.
+        You have two different agents: 
+            1. sales agent - One is a sales agent who is expert in handling onboarding new customers, helping them understand the existing plans, and helping them choose the better plan.
+            2. Refund Agent - There is another agent who is expert in issuing user refunds when the users are not happy with the service.`,
+    handoffDescription: `You are an expert customer-facing agent who understands user queries and gracefully handoff their request to the respective agents.
+        You have two different agents:
+            1. sales agent - One is a sales agent who is expert in handling onboarding new customers, helping them understand the existing plans, and helping them choose the better plan.
+            2. Refund Agent - There is another agent who is expert in issuing user refunds when the users are not happy with the service.`,
+    handoffs : [salesAgent,refundAgent]
+})
+
 
 async function runAgent(query = "") {
-    const agentResponse = await run(salesAgent, query)
+    const agentResponse = await run(customerFacingAgent, query)
     console.log("Reply ----> ", agentResponse.finalOutput)
 }
 
@@ -68,3 +82,8 @@ async function runAgent(query = "") {
 runAgent("Can you please let me know your current plans which has unlimited usage?")
 
 // runAgent("I have a 399 plan and i am not happy with the service as it keeps disconnecting every now and then, i would need a refund and my customer id is II-29")
+
+
+
+
+
